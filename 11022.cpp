42a0bd1d3d45fd5239f2cf8f24d15207d352aef9
Seungmin Kang <autumnsky1562@gmail.com>
2026-03-20 15:44:01 +0900

    반복문: 11022 문제 추가

---
+++ 11022.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n = 0;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    int a = 0, b = 0;
+    cin >> a >> b;
+
+    cout << "Case #" << i << ": " << a << " + " << b << " = " << a+b << "\n";
+  }
+
+  return 0;
+}
