37232b55cc43351b72f5edaf036af1393d3a330f
Autumnsky-K <autumnsky1562@gmail.com>
2026-03-30 10:54:45 +0900

    문자열: 27866, 2743 문제 추가
    - 입력을 받아 특정 인덱스의 문자 출력
    - 입력을 받아 해당 문자열의 길이 출력

---
+++ 27866.cpp
@@ -0,0 +1,19 @@
+#include <ios>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+
+    int i;
+    cin >> i;
+
+    cout << s[i-1];
+
+    return 0;
+}
